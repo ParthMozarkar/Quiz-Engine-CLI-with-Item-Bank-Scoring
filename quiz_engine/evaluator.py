bf3f7725eafdf6ff4fc6549dd4fa45fc18670441
Parth Mozarkar <greatparth21@gmail.com>
2025-12-02 15:41:42 +0530

    made the evaluator work with the ui

--- quiz_engine/evaluator.py
+++ quiz_engine/evaluator.py
@@ -1,29 +1,6 @@
-def ask_question(quesobj):
-    print("\n" + quesobj["question"])
+# 
 
-    options = quesobj["options"]
-    for i, opt in enumerate(options):
-        print(f"{chr(65+i)}) {opt}")
-
-    while True:
-        user_ans = input("Your answer: ").strip().upper()
-
-        if not user_ans.isalpha() or len(user_ans) != 1:
-            print("Invalid input, please enter A, B, C, or D.")
-            continue 
-
-        chosen = ord(user_ans) - 65
-
-        if chosen < 0 or chosen >= len(options):
-            print("Invalid input, please choose A, B, C, or D.")
-            continue 
-
-        break 
-
-    correct = quesobj["answer"]
-
-    if chosen == correct:
-        return True
-    
-    else:
+def evaluate_answer(selected_index,correct_index):
+    if selected_index == -1:
         return False
+    return selected_index == correct_index
